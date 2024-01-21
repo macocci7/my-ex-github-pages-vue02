9ae824de44400ccdc80b953b2fc9242bf7481273
macocci7 <macocci7@yahoo.co.jp>
2024-01-21 13:37:59 +0900

    Repo Pages

--- vite.config.js
+++ vite.config.js
@@ -13,8 +13,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: './',
-  build: {
-    outDir: 'docs'
-  }
+  base: '/my-ex-github-pages-vue02/'
 })
